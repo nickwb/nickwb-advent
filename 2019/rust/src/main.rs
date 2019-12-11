f7a127ddd65df2ff5abf071ff86d9e56588f2eff
Nick Young <nick.young@stacktracehq.com>
2019-12-11 17:45:18 +1000

    WIP; Day 6

--- 2019/rust/src/main.rs
+++ 2019/rust/src/main.rs
@@ -5,6 +5,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 #[macro_use]
 extern crate lazy_static;
@@ -15,4 +16,6 @@ fn main() {
     day3::run_day_three();
     day4::run_day_four();
     day5::run_day_five();
+    day5::run_day_five();
+    day6::run_day_six();
 }
