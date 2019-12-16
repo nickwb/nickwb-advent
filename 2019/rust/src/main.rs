b9dd977a1bae0bca28625ac9f9fc8ce6224002e4
Nick Young <nick@nickwb.net>
2019-12-16 20:43:38 +1000

    Day 7, Part 1

--- 2019/rust/src/main.rs
+++ 2019/rust/src/main.rs
@@ -6,9 +6,13 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 #[macro_use]
 extern crate lazy_static;
+
+#[macro_use]
+extern crate itertools;
 
 fn main() {
     day1::run_day_one();
@@ -17,4 +21,5 @@ fn main() {
     day4::run_day_four();
     day5::run_day_five();
     day6::run_day_six();
+    day7::run_day_seven();
 }
