702d6839a894199fa18be0a6e9f8ad3daf1b516f
Nick Young <nick@nickwb.net>
2020-09-12 22:08:17 +1000

    Day 13, Part 2. Implement the game.

--- 2019/rust/src/bin/day13-game.rs
+++ 2019/rust/src/bin/day13-game.rs
@@ -1,5 +1,32 @@
-use nickwb_advent::day13;
+use std::io::Write;
+
+use nickwb_advent::day13::GameState;
 
 fn main() {
-    day13::run_day_thirteen();
+    let mut state = GameState::new();
+    let mut move_input = String::with_capacity(5);
+    let mut is_cheating = false;
+
+    while !state.run_one_cycle() {
+        print!("Move: ");
+        std::io::stdout().lock().flush().unwrap();
+        std::io::stdin().read_line(&mut move_input).unwrap();
+        match move_input.trim() {
+            "a" => state.buffer_left(),
+            "d" => state.buffer_right(),
+            "x" => {
+                is_cheating = !is_cheating;
+                state.buffer_optimal();
+            }
+            _ => {
+                if is_cheating {
+                    state.buffer_optimal()
+                } else {
+                    state.buffer_neutral()
+                }
+            }
+        }
+        move_input.clear();
+        println!("");
+    }
 }
--- 2019/rust/src/day13.rs
+++ 2019/rust/src/day13.rs
@@ -28,6 +28,7 @@ impl CellType {
 struct ScreenBuffer {
     cells: HashMap<Point, CellType>,
     state: SinkState,
+    last_score: MemoryCell,
 }
 
 impl ScreenBuffer {
@@ -35,7 +36,46 @@ impl ScreenBuffer {
         ScreenBuffer {
             cells: HashMap::new(),
             state: SinkState::ZeroReceived,
+            last_score: 0,
         }
+    }
+
+    fn render_frame(&self) -> (Option<Point>, Option<Point>) {
+        let (min_x, max_x, min_y, max_y) =
+            self.cells.keys().fold((0, 0, 0, 0), |extremes, point| {
+                (
+                    extremes.0.min(point.x),
+                    extremes.1.max(point.x),
+                    extremes.2.min(point.y),
+                    extremes.3.max(point.y),
+                )
+            });
+
+        let mut paddle_at: Option<Point> = None;
+        let mut ball_at: Option<Point> = None;
+
+        for y in min_y..=max_y {
+            for x in min_x..=max_x {
+                let char = match self.cells.get(&Point::xy(x, y)) {
+                    Some(CellType::Ball) => {
+                        ball_at = Some(Point::xy(x, y));
+                        'O'
+                    }
+                    Some(CellType::Block) => 'X',
+                    Some(CellType::Wall) => '|',
+                    Some(CellType::Paddle) => {
+                        paddle_at = Some(Point::xy(x, y));
+                        '^'
+                    }
+                    _ => ' ',
+                };
+                print!("{}", char);
+            }
+            println!("");
+        }
+
+        println!("Score: {}", self.last_score);
+        (paddle_at, ball_at)
     }
 }
 
@@ -51,13 +91,82 @@ impl OutputSink for ScreenBuffer {
             SinkState::ZeroReceived => SinkState::OneReceived(value),
             SinkState::OneReceived(x) => SinkState::TwoReceived(x, value),
             SinkState::TwoReceived(x, y) => {
-                let point = Point::xy(x, y);
-                let cell_type = CellType::from_numeric(value);
-                self.cells.insert(point, cell_type);
+                if x == -1 && y == 0 {
+                    self.last_score = value;
+                } else {
+                    let point = Point::xy(x, y);
+                    let cell_type = CellType::from_numeric(value);
+                    self.cells.insert(point, cell_type);
+                }
                 SinkState::ZeroReceived
             }
         };
         self.state = new_state;
+    }
+}
+
+pub struct GameState {
+    comp: Computer<Vec<MemoryCell>, BufferInput, ScreenBuffer>,
+    paddle_at: Point,
+    ball_at: Point,
+}
+
+impl GameState {
+    pub fn new() -> GameState {
+        let input = BufferInput::new(1);
+        let output = ScreenBuffer::new();
+        let mut state = inputs();
+        state[0] = 2;
+        let mut computer = Computer::new(state, input, output);
+        computer.enable_extra_memory();
+
+        GameState {
+            comp: computer,
+            paddle_at: Point::xy(0, 0),
+            ball_at: Point::xy(0, 0),
+        }
+    }
+
+    pub fn run_one_cycle(&mut self) -> bool {
+        let game_over = match self.comp.resume() {
+            Ok(StepResult::WaitingOnInput) => false,
+            Ok(StepResult::Halt) => true,
+            _ => panic!("The computer got in to an invalid state"),
+        };
+
+        let (paddle_at, ball_at) = self.comp.output().render_frame();
+
+        if let Some(paddle) = paddle_at {
+            self.paddle_at = paddle;
+        }
+
+        if let Some(ball) = ball_at {
+            self.ball_at = ball;
+        }
+
+        game_over
+    }
+
+    pub fn buffer_left(&mut self) {
+        self.comp.input().queue(-1);
+    }
+
+    pub fn buffer_right(&mut self) {
+        self.comp.input().queue(1);
+    }
+
+    pub fn buffer_neutral(&mut self) {
+        self.comp.input().queue(0);
+    }
+
+    pub fn buffer_optimal(&mut self) {
+        if self.paddle_at.x < self.ball_at.x {
+            self.buffer_right();
+        } else if self.paddle_at.x > self.ball_at.x {
+            self.buffer_left();
+        } else {
+            self.buffer_neutral();
+        }
     }
 }
 
