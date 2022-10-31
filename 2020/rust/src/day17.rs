372d3a21b4e1d8724a26048e9fee9a130d5b717f
Nick Young <nick@nickwb.net>
2022-10-31 22:02:54 +1000

    2020; Day 17 Part 2

--- 2020/rust/src/day17.rs
+++ 2020/rust/src/day17.rs
@@ -1,9 +1,10 @@
 use std::{collections::HashMap, hash::Hash};
 
 pub fn run_day_seventeen() {
-    let grid = inputs();
-    println!("Day 17, Part 1: {}", calculate_part_1(grid));
-    //println!("Day 17, Part 2: {}", calculate_part_2(&mut inputs));
+    let grid_one = inputs();
+    let grid_two = grid_one.clone();
+    println!("Day 17, Part 1: {}", calculate_part_1(grid_one));
+    println!("Day 17, Part 2: {}", calculate_part_2(grid_two));
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
@@ -11,11 +12,14 @@ struct Point {
     pub x: i32,
     pub y: i32,
     pub z: i32,
+    pub w: i32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct ActiveGrid {
     cells: Vec<Point>,
+
+    // candidates and swap_cells are just collections that we recycle to avoid their re-allocation
     candidates: HashMap<Point, usize>,
     swap_cells: Option<Vec<Point>>,
 }
@@ -24,7 +28,7 @@ impl ActiveGrid {
     fn from_inputs(cells: Vec<Point>) -> Self {
         Self {
             cells,
-            candidates: HashMap::with_capacity(26),
+            candidates: HashMap::with_capacity(80),
             swap_cells: None,
         }
     }
@@ -33,14 +37,18 @@ impl ActiveGrid {
 fn calculate_part_1(input: ActiveGrid) -> usize {
     let mut grid = input;
     for _ in 0..6 {
-        grid = run_single_conway_cycle(grid)
+        grid = run_conway_cycle(grid, true)
     }
     grid.cells.len()
 }
 
-// fn calculate_part_2(input: ActiveGrid) -> usize {
-//     0
-// }
+fn calculate_part_2(input: ActiveGrid) -> usize {
+    let mut grid = input;
+    for _ in 0..6 {
+        grid = run_conway_cycle(grid, false)
+    }
+    grid.cells.len()
+}
 
 fn parse_initial_grid(str: &str) -> ActiveGrid {
     let cells = str
@@ -57,13 +65,14 @@ fn parse_initial_grid(str: &str) -> ActiveGrid {
             x: x as i32,
             y: y as i32,
             z: 0,
+            w: 0,
         })
         .collect::<Vec<Point>>();
 
     ActiveGrid::from_inputs(cells)
 }
 
-fn run_single_conway_cycle(grid: ActiveGrid) -> ActiveGrid {
+fn run_conway_cycle(grid: ActiveGrid, suppress_w: bool) -> ActiveGrid {
     let ActiveGrid {
         mut cells,
         mut candidates,
@@ -72,36 +81,48 @@ fn run_single_conway_cycle(grid: ActiveGrid) -> ActiveGrid {
 
     let mut next = swap_cells.unwrap_or(Vec::new());
 
-    for (i, a) in cells.iter().enumerate() {
+    for (i, cell) in cells.iter().enumerate() {
         // Find candidate neighbors that could be promoted to active
-        for x in a.x - 1..=a.x + 1 {
-            for y in a.y - 1..=a.y + 1 {
-                for z in a.z - 1..=a.z + 1 {
-                    if x == a.x && y == a.y && z == a.z {
-                        continue;
-                    }
+        for x in cell.x - 1..=cell.x + 1 {
+            for y in cell.y - 1..=cell.y + 1 {
+                for z in cell.z - 1..=cell.z + 1 {
+                    let w_range = if suppress_w {
+                        (0, 0)
+                    } else {
+                        (cell.w - 1, cell.w + 1)
+                    };
+                    for w in w_range.0..=w_range.1 {
+                        // This check is not technically necessary, given that we cull the
+                        // candidates list later. But a quick benchmark suggests that including
+                        // this check is marginally more efficient.
+                        if x == cell.x && y == cell.y && z == cell.z && w == cell.w {
+                            continue;
+                        }
 
-                    candidates
-                        .entry(Point { x, y, z })
-                        .and_modify(|n| *n += 1)
-                        .or_insert(1);
+                        candidates
+                            .entry(Point { x, y, z, w })
+                            .and_modify(|n| *n += 1)
+                            .or_insert(1);
+                    }
                 }
             }
         }
 
         // Check if this cell stays active by checking whether it neighbors other active cells
         let mut n = 0;
-        for (j, b) in cells.iter().enumerate() {
+        for (j, other) in cells.iter().enumerate() {
             if i == j {
                 continue;
             }
 
-            if b.x >= a.x - 1
-                && b.x <= a.x + 1
-                && b.y >= a.y - 1
-                && b.y <= a.y + 1
-                && b.z >= a.z - 1
-                && b.z <= a.z + 1
+            if other.x >= cell.x - 1
+                && other.x <= cell.x + 1
+                && other.y >= cell.y - 1
+                && other.y <= cell.y + 1
+                && other.z >= cell.z - 1
+                && other.z <= cell.z + 1
+                && other.w >= cell.w - 1
+                && other.w <= cell.w + 1
             {
                 n += 1;
             }
@@ -109,7 +130,7 @@ fn run_single_conway_cycle(grid: ActiveGrid) -> ActiveGrid {
 
         // Does this cell remain active?
         if n == 2 || n == 3 {
-            next.push(*a);
+            next.push(*cell);
         }
     }
 
@@ -153,13 +174,17 @@ mod tests {
             ###
         ";
 
-        let grid = parse_initial_grid(text);
-        assert_eq!(112, calculate_part_1(grid));
+        let grid_one = parse_initial_grid(text);
+        let grid_two = grid_one.clone();
+        assert_eq!(112, calculate_part_1(grid_one));
+        assert_eq!(848, calculate_part_2(grid_two));
     }
 
     #[test]
     fn actual_inputs() {
-        let grid = inputs();
-        assert_eq!(291, calculate_part_1(grid));
+        let grid_one = inputs();
+        let grid_two = grid_one.clone();
+        assert_eq!(291, calculate_part_1(grid_one));
+        assert_eq!(1524, calculate_part_2(grid_two));
     }
 }
