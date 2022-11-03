0ef362f37c06762490cd5b629ce67dfe7a2d086f
Nick Young <nick@nickwb.net>
2022-11-03 22:05:14 +1000

    Day 18 - Part 1; 2020

--- 2020/rust/src/day18.rs
+++ 2020/rust/src/day18.rs
@@ -6,26 +6,30 @@ use nom::{
     sequence::{delimited, pair},
     IResult,
 };
-use regex::internal::Input;
+use once_cell::sync::Lazy;
 
 pub fn run_day_eighteen() {
-    // let grid_one = inputs();
-    // println!("Day 18, Part 1: {}", calculate_part_1(grid_one));
+    let inputs = inputs();
+    println!("Day 18, Part 1: {}", calculate_part_1(&inputs));
     // println!("Day 18, Part 2: {}", calculate_part_2(grid_two));
 }
 
-fn bracket_expr(i: &str) -> IResult<&str, i64> {
-    delimited(complete::char('('), expr_parser, complete::char(')'))(i)
-}
-
-fn atom(i: &str) -> IResult<&str, i64> {
-    let brackets_or_num = alt((bracket_expr, complete::i64));
-    delimited(space0, brackets_or_num, space0)(i)
+pub fn calculate_part_1<T: AsRef<str>>(lines: &[T]) -> i64 {
+    lines.iter().map(|s| parse_and_evaluate(s.as_ref())).sum()
 }
 
 type Op = fn(i64, i64) -> i64;
 
-fn op_parser(i: &str) -> IResult<&str, Op> {
+fn bracketed_expression(i: &str) -> IResult<&str, i64> {
+    delimited(complete::char('('), expression, complete::char(')'))(i)
+}
+
+fn atomic_operand(i: &str) -> IResult<&str, i64> {
+    let brackets_or_num = alt((bracketed_expression, complete::i64));
+    delimited(space0, brackets_or_num, space0)(i)
+}
+
+fn operator(i: &str) -> IResult<&str, Op> {
     let symbol = alt((complete::char('+'), complete::char('*')));
 
     fn add(a: i64, b: i64) -> i64 {
@@ -43,26 +47,26 @@ fn op_parser(i: &str) -> IResult<&str, Op> {
     })(i)
 }
 
-fn expr_parser(i: &str) -> IResult<&str, i64> {
+fn expression(i: &str) -> IResult<&str, i64> {
     // Get the first operand
-    let (remaining, first) = atom(i)?;
+    let (remaining, first) = atomic_operand(i)?;
 
     // Get pairs of operators and operands
-    let part = pair(op_parser, atom);
+    let part = pair(operator, atomic_operand);
 
     // Apply the pairs left to right
     fold_many0(part, move || first, |lhs, (op, rhs)| op(lhs, rhs))(remaining)
 }
 
-fn parse_and_evalute(expr: &str) -> i64 {
-    let (_, o) = all_consuming(expr_parser)(expr).unwrap();
+fn parse_and_evaluate(expr: &str) -> i64 {
+    let (_, o) = all_consuming(expression)(expr).unwrap();
     o
 }
 
-// fn inputs() -> ActiveGrid {
-//     let text = crate::util::read_file("inputs/day17.txt");
-//     parse_initial_grid(&text)
-// }
+fn inputs() -> Vec<String> {
+    let text = crate::util::read_file("inputs/day18.txt");
+    text.lines().map(|s| s.to_owned()).collect()
+}
 
 #[cfg(test)]
 mod tests {
@@ -70,24 +74,24 @@ mod tests {
 
     #[test]
     fn example_1() {
-        assert_eq!(71, parse_and_evalute("1 + 2 * 3 + 4 * 5 + 6"));
+        assert_eq!(71, parse_and_evaluate("1 + 2 * 3 + 4 * 5 + 6"));
     }
 
     #[test]
     fn example_2() {
-        assert_eq!(26, parse_and_evalute("2 * 3 + (4 * 5)"));
+        assert_eq!(26, parse_and_evaluate("2 * 3 + (4 * 5)"));
     }
 
     #[test]
     fn example_3() {
-        assert_eq!(437, parse_and_evalute("5 + (8 * 3 + 9 + 3 * 4 * 3)"));
+        assert_eq!(437, parse_and_evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)"));
     }
 
     #[test]
     fn example_4() {
         assert_eq!(
             12240,
-            parse_and_evalute("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")
+            parse_and_evaluate("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")
         );
     }
 
@@ -95,15 +99,14 @@ mod tests {
     fn example_5() {
         assert_eq!(
             13632,
-            parse_and_evalute("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
+            parse_and_evaluate("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
         );
     }
 
-    // #[test]
-    // fn actual_inputs() {
-    //     let grid_one = inputs();
-    //     let grid_two = grid_one.clone();
-    //     assert_eq!(291, calculate_part_1(grid_one));
-    //     assert_eq!(1524, calculate_part_2(grid_two));
-    // }
+    #[test]
+    fn actual_inputs() {
+        let inputs = inputs();
+        assert_eq!(4491283311856, calculate_part_1(&inputs));
+        // assert_eq!(1524, calculate_part_2(grid_two));
+    }
 }
