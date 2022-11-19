b6af6d221efacfb66bc91e3492ec421af181869d
Nick Young <nick@nickwb.net>
2022-11-19 16:34:12 +1000

    2020 - Day 19

--- 2020/rust/src/day19.rs
+++ 2020/rust/src/day19.rs
@@ -1,11 +1,5 @@
-use std::{
-    borrow::Cow,
-    cell::Cell,
-    collections::{HashMap, HashSet},
-    fmt,
-};
+use std::collections::HashMap;
 
-use itertools::Itertools;
 use nom::{
     branch::alt,
     character::complete::{self, alpha1, line_ending, multispace0, satisfy, space0},
@@ -41,15 +35,6 @@ struct MessageRule {
 struct Day19 {
     rules: HashMap<RuleNum, MessageRule>,
     messages: Vec<String>,
-    enable_verbose: Cell<bool>,
-}
-
-impl Day19 {
-    pub fn verbose_log(&self, args: fmt::Arguments) {
-        if self.enable_verbose.get() {
-            eprintln!("{}", args);
-        }
-    }
 }
 
 fn inputs() -> Day19 {
@@ -80,332 +65,190 @@ fn calculate_part_2(input: &mut Day19) -> usize {
     input.messages.iter().filter(|m| is_match(input, m)).count()
 }
 
-const VERBOSE_FOR: [&'static str; 3] = [
-    "bbbbbbbaaaabbbbaaabbabaaa",
-    "bbbababbbbaaaaaaaabbababaaababaabab",
-    "abbbbabbbbaaaababbbbbbaaaababb",
-];
-
 fn is_match(model: &Day19, message: &str) -> bool {
-    let mut alternatives = AlternativesState {
-        failed: HashSet::new(),
-        maybe: HashSet::new(),
+    let state = SearchState {
+        input_model: model,
+        _message: message,
+    };
+    let candidate = Candidate {
+        choice_depth: 0,
+        choices: 0,
+        consumed: 0,
+        remaining: message,
         stack: Vec::new(),
     };
-
-    if VERBOSE_FOR.contains(&message) {
-        model.enable_verbose.set(true);
-    } else {
-        model.enable_verbose.set(false);
-    }
-
-    loop {
-        match evaluate_rule(0, &message, 0, model, "", &mut alternatives) {
-            None => {
-                eprintln!("Message {} is not a match", message);
-                return false;
-            }
-            Some(result) => {
-                if result.remaining.is_empty() {
-                    eprintln!("Message {} is a match", message);
-                    return true;
-                }
-                if result.is_exhausted {
-                    eprintln!(
-                        "Message {} is not a match. Leftover: {}",
-                        message, result.remaining
-                    );
-
-                    return false;
-                }
-                eprintln!("Rule 0 was not exhausted...");
-            }
-        }
-    }
+    search(&state, candidate, 0)
 }
 
-struct EvaluateResult<'a> {
-    remaining: &'a str,
-    is_exhausted: bool,
-    path_update: Option<String>,
+struct SearchState<'im> {
+    input_model: &'im Day19,
+    _message: &'im str,
 }
 
-#[derive(PartialEq)]
-struct Candidate {
-    pos: usize,
-    rule_num: RuleNum,
-}
-
-struct AlternativesState {
-    failed: HashSet<String>,
-    maybe: HashSet<String>,
-    stack: Vec<Candidate>,
-}
-
-// Recursively evaluates rules on the supplied text.
-// On a successful match, it returns the remaining (unmatched) text.
-// On an unsuccessful match, it returns None
-fn evaluate_rule<'a>(
-    pos: usize,
-    text: &'a str,
-    rule_num: RuleNum,
-    model: &Day19,
-    path: &str,
-    alternatives: &mut AlternativesState,
-) -> Option<EvaluateResult<'a>> {
-    if text.is_empty() {
-        return None;
-    }
-
-    let unwind_stack_to = alternatives.stack.len();
-    let candidate = Candidate { pos, rule_num };
-
-    if alternatives.stack.iter().rev().contains(&candidate) {
-        eprintln!("Avoiding infinite recursion...");
-        return None;
-    } else {
-        alternatives.stack.push(candidate);
-    }
-
-    let rule = model
-        .rules
-        .get(&rule_num)
-        .expect("The rule number to exist");
-
-    let result = match &rule.spec {
-        RuleSpec::Literal(c) => {
-            if text.starts_with(*c) {
-                Some(EvaluateResult {
-                    remaining: &text[1..],
-                    is_exhausted: true,
-                    path_update: None,
-                })
-            } else {
-                None
-            }
-        }
-        RuleSpec::Sequence(rule_nums) => {
-            evaluate_sequence(pos, text, model, path, alternatives, &rule_nums)
-        }
-        RuleSpec::Alternation((sequence_x, sequence_y)) => evaluate_alternation(
-            pos,
-            text,
-            model,
-            path,
-            alternatives,
-            &sequence_x,
-            &sequence_y,
-        ),
-    };
-
-    while alternatives.stack.len() > unwind_stack_to {
-        alternatives.stack.pop();
-    }
-
-    result
-}
-
-fn evaluate_alternation<'a>(
-    pos: usize,
-    text: &'a str,
-    model: &Day19,
-    path: &str,
-    alternatives: &mut AlternativesState,
-    sequence_x: &[RuleNum],
-    sequence_y: &[RuleNum],
-) -> Option<EvaluateResult<'a>> {
-    let x_path = format!("{}X", path);
-    let y_path = format!("{}Y", path);
-
-    let x_failed = alternatives.failed.contains(&x_path);
-    let y_failed = alternatives.failed.contains(&y_path);
-    let x_maybe = alternatives.maybe.contains(&x_path);
-    let y_maybe = alternatives.maybe.contains(&y_path);
-
-    let (skip_x, skip_y) = match (x_failed, y_failed, x_maybe, y_maybe) {
-        (false, false, false, false) => (false, false), // 0000 Nothing tried
-        (false, false, false, true) => unreachable!(),  // 0001
-        (false, false, true, false) => (true, false),   // 0010 X works, but Y not yet tried
-        (false, false, true, true) => {
-            todo!()
-        }
-        (false, true, false, false) => unreachable!(), // 0100
-        (false, true, false, true) => unreachable!(),  // 0101
-        (false, true, true, false) => (false, true),   // 0110 Only X works
-        (false, true, true, true) => unreachable!(),   // 0111
-        (true, false, false, false) => (true, false),  // 1000 X does not work, Y not yet tried
-        (true, false, false, true) => (true, false),   // 1001 X does not work, Y does work
-        (true, false, true, false) => unreachable!(),  // 1010
-        (true, false, true, true) => unreachable!(),   // 1011
-        (true, true, false, false) => (false, false),  // 1100 Neither work
-        (true, true, false, true) => unreachable!(),   // 1101
-        (true, true, true, false) => unreachable!(),   // 1110
-        (true, true, true, true) => unreachable!(),    // 1111
-    };
-
-    let x_result = if skip_x {
-        model.verbose_log(format_args!(
-            "Returned to path: {} at position {} with remainder: {}",
-            &x_path, pos, text,
-        ));
-        None
-    } else {
-        evaluate_sequence(pos, text, model, &x_path, alternatives, sequence_x)
-    };
-
-    match x_result {
-        Some(result) => {
-            // let path_update = if result.is_exhausted {
-            //     let u = result.path_update.unwrap_or_else(|| x_path.clone());
-            //     model.verbose_log(format_args!(
-            //         "Excluding exhausted path: {} at position {} with remainder: {}",
-            //         &x_path, pos, result.remaining,
-            //     ));
-            //     alternatives.maybe.insert(x_path);
-            //     u
-            // } else {
-            //     result.path_update.unwrap_or(x_path)
-            // };
-            let path_update = result.path_update.unwrap_or_else(|| x_path.clone());
-            if !x_maybe {
-                alternatives.maybe.insert(x_path);
-            }
-
-            Some(EvaluateResult {
-                remaining: result.remaining,
-                is_exhausted: y_failed && result.is_exhausted,
-                path_update: Some(path_update),
-            })
-        }
-        None => {
-            if !skip_x {
-                alternatives.failed.insert(x_path);
-            }
-
-            if skip_y {
-                model.verbose_log(format_args!(
-                    "Returned to path: {} at position {} with remainder: {}",
-                    &y_path, pos, text,
-                ));
-                return None;
-            }
-
-            let y_result = evaluate_sequence(pos, text, model, &y_path, alternatives, sequence_y);
-
-            match y_result {
-                Some(result) => {
-                    // let path_update = if result.is_exhausted {
-                    //     let u = result.path_update.unwrap_or_else(|| y_path.clone());
-                    //     model.verbose_log(format_args!(
-                    //         "Excluding exhausted path: {} at position {} with remainder: {}",
-                    //         &y_path, pos, result.remaining,
-                    //     ));
-                    //     alternatives.maybe.insert(y_path);
-                    //     u
-                    // } else {
-                    //     result.path_update.unwrap_or(y_path)
-                    // };
-                    let path_update = result.path_update.unwrap_or_else(|| y_path.clone());
-                    if !y_maybe {
-                        alternatives.maybe.insert(y_path);
-                    }
-
-                    Some(EvaluateResult {
-                        remaining: result.remaining,
-                        is_exhausted: x_failed && result.is_exhausted,
-                        path_update: Some(path_update),
-                    })
-                }
-                None => {
-                    if !skip_y {
-                        alternatives.failed.insert(y_path);
-                    }
-                    None
-                }
-            }
-        }
-    }
-}
-
-fn evaluate_sequence<'a, 'b>(
-    pos: usize,
-    text: &'a str,
-    model: &Day19,
-    path: &'b str,
-    alternatives: &mut AlternativesState,
-    rule_number_sequence: &[RuleNum],
-) -> Option<EvaluateResult<'a>> {
-    #[derive(Clone)]
-    struct FoldState<'a, 'b> {
-        text: &'a str,
-        pos: usize,
+#[derive(Clone, Copy)]
+enum Bookmark {
+    Sequence {
+        rule: RuleNum,
         idx: usize,
-        path: Cow<'b, str>,
+    },
+    AltSequence {
+        rule: RuleNum,
+        branch: u8,
+        idx: usize,
+    },
+}
+
+/// Represents a partially matched message
+#[derive(Clone)]
+struct Candidate<'im> {
+    remaining: &'im str,
+    stack: Vec<Bookmark>,
+    // For debugging: How much of the message have we parsed already
+    consumed: u16,
+    // For debugging: A bitstring of all of the branches we have taken
+    choices: u128,
+    // For debugging: The number of branches we have taken so far
+    choice_depth: u8,
+}
+
+impl<'im> Candidate<'im> {
+    pub fn next_char(&self) -> Option<char> {
+        self.remaining.chars().next()
     }
 
-    let mut state = FoldState {
-        text,
-        pos,
-        idx: 0,
-        path: Cow::Borrowed(path),
-    };
-    let mut backtracks: Vec<FoldState> = Vec::new();
+    pub fn advance_one(&mut self) {
+        self.consumed += 1;
+        self.remaining = &self.remaining[1..];
+    }
 
-    'rules: loop {
-        let rule_num = rule_number_sequence[state.idx];
+    pub fn make_choice(&mut self, x_branch: bool) {
+        if !x_branch {
+            let mask = 1 << (self.choice_depth as u128);
+            self.choices = self.choices & mask;
+        }
+        self.choice_depth += 1;
+    }
+}
 
-        let sub_result = evaluate_rule(
-            state.pos,
-            state.text,
-            rule_num,
-            model,
-            &state.path,
-            alternatives,
-        );
+// This is a recursive search, which tries to match the whole of the remaining message.
+// This method starts at a given rule number, and continues matching until either the whole message
+// has been matched, or we conclude that a message does not match.
+// Alternations are tricky. Locally, it may be true that BOTH branches can be successfully matched,
+// but this may not be true globally: as different branches may consume different amounts of input.
+// This is why this recursion matches the whole of the remaining input.
+// Arising from this construction is the bookmark system: our way of notating rule sequences which
+// have only been partially evaluated. Once we reach a leaf rule (a literal), we continue our
+// recursion deeper by calling resume, which will pop bookmarks off the stack and continue the
+// search until we have finished matching every rule and every character in the message.
+fn search<'im>(state: &'im SearchState<'im>, mut candidate: Candidate<'im>, rule: RuleNum) -> bool {
+    let spec = &state.input_model.rules[&rule].spec;
 
-        if let None = sub_result {
-            if let Some(pop_state) = backtracks.pop() {
-                state = pop_state;
-                continue 'rules;
+    match spec {
+        RuleSpec::Literal(c) => {
+            if *c == candidate.next_char().expect("More input to consume") {
+                candidate.advance_one();
+
+                // This character is a match, but we must now continue searching
+                // through partially evaluated rules to confirm the whole match
+                resume(state, candidate)
             } else {
-                return None;
+                // This candidate does not match
+                false
             }
         }
-
-        let sub_result = sub_result.unwrap();
-
-        if !sub_result.is_exhausted {
-            backtracks.push(state.clone());
+        RuleSpec::Sequence(rules) => {
+            if rules.len() > 1 {
+                // There's more than one rule in this sequence, so we'll need to return to the
+                // subsequent rules after the first index is evaluated
+                candidate.stack.push(Bookmark::Sequence { rule, idx: 1 });
+            }
+            search(state, candidate, rules[0])
         }
+        RuleSpec::Alternation((rules_x, rules_y)) => {
+            let mut candidate_x = candidate.clone();
+            candidate_x.make_choice(true);
 
-        // The sub rule successfully matched with an alternation, so update our path
-        if let Some(path) = sub_result.path_update {
-            state.path = Cow::Owned(path);
-        }
+            if rules_x.len() > 1 {
+                // There's more than one rule in this sequence, so we'll need to return to the
+                // subsequent rules after the first index is evaluated
+                candidate_x.stack.push(Bookmark::AltSequence {
+                    rule,
+                    branch: 0,
+                    idx: 1,
+                });
+            }
+            let result_x = search(state, candidate_x, rules_x[0]);
 
-        let advanced_by = state.text.len() - sub_result.remaining.len();
+            if result_x {
+                // The first branch was a complete match, so we don't need to search the other branch
+                true
+            } else {
+                let mut candidate_y = candidate;
+                candidate_y.make_choice(false);
 
-        state.pos += advanced_by;
-        state.text = sub_result.remaining;
-        state.idx += 1;
-
-        // We've finished matching the sequence successfully
-        if state.idx == rule_number_sequence.len() {
-            break;
+                if rules_y.len() > 1 {
+                    // There's more than one rule in this sequence, so we'll need to return to the
+                    // subsequent rules after the first index is evaluated
+                    candidate_y.stack.push(Bookmark::AltSequence {
+                        rule,
+                        branch: 1,
+                        idx: 1,
+                    });
+                }
+                search(state, candidate_y, rules_y[0])
+            }
         }
     }
+}
 
-    let path_update = match state.path {
-        Cow::Borrowed(b) if b == path => None,
-        Cow::Borrowed(b) => Some(b.to_owned()), // Paranoia, but shouldn't ever happen
-        Cow::Owned(o) => Some(o),
-    };
+// Continue searching for a match on the given candidate, resolving any suspended rules on the stack
+fn resume<'im>(state: &'im SearchState<'im>, mut candidate: Candidate<'im>) -> bool {
+    let has_more_input = !candidate.remaining.is_empty();
+    let has_more_rules = !candidate.stack.is_empty();
 
-    Some(EvaluateResult {
-        remaining: state.text,
-        is_exhausted: backtracks.is_empty(),
-        path_update: path_update,
-    })
+    // First check if we are either fully matched, or we have reached a failure state
+    match (has_more_input, has_more_rules) {
+        (false, false) => return true,
+        (true, false) | (false, true) => return false,
+        (true, true) => {}
+    }
+
+    // We didn't finish evaluating this rule:
+    let resume_to = candidate.stack.pop().expect("More rules to match");
+
+    match resume_to {
+        Bookmark::Sequence { rule, idx } => {
+            if let RuleSpec::Sequence(seq) = &state.input_model.rules[&rule].spec {
+                let is_last = idx + 1 == seq.len();
+                if !is_last {
+                    // Still not at the end
+                    candidate
+                        .stack
+                        .push(Bookmark::Sequence { rule, idx: idx + 1 });
+                }
+                search(state, candidate, seq[idx])
+            } else {
+                panic!("Sequence bookmark wasn't a sequence")
+            }
+        }
+        Bookmark::AltSequence { rule, branch, idx } => {
+            if let RuleSpec::Alternation((seq_x, seq_y)) = &state.input_model.rules[&rule].spec {
+                let seq = if branch == 0 { seq_x } else { seq_y };
+                let is_last = idx + 1 == seq.len();
+                if !is_last {
+                    // Still not at the end
+                    candidate.stack.push(Bookmark::AltSequence {
+                        rule,
+                        branch,
+                        idx: idx + 1,
+                    });
+                }
+                search(state, candidate, seq[idx])
+            } else {
+                panic!("AltSequence bookmark wasn't an Alternation")
+            }
+        }
+    }
 }
 
 fn input_parser<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Day19> {
@@ -460,7 +303,6 @@ fn input_parser<'a>() -> impl FnMut(&'a str) -> IResult<&'a str, Day19> {
         tuple((parse_all_rules, parse_blank_line, parse_all_messages)).map(|(r, _, m)| Day19 {
             rules: r.into_iter().map(|r| (r.num, r)).collect(),
             messages: m,
-            enable_verbose: Cell::new(false),
         });
 
     all_consuming(delimited(multispace0, parse_all_inputs, multispace0))
@@ -549,8 +391,8 @@ mod tests {
 
     #[test]
     fn actual_inputs() {
-        let inputs = inputs();
+        let mut inputs = inputs();
         assert_eq!(113, calculate_part_1(&inputs));
-        // assert_eq!(68852578641904, calculate_part_2(&inputs));
+        assert_eq!(253, calculate_part_2(&mut inputs));
     }
 }
