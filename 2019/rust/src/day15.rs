5c7f08395bda2be0d57549fa028a3fc926eb47e0
Nick Young <nick@nickwb.net>
2022-12-11 14:27:56 +1000

    WIP; 2019; Day 15

--- 2019/rust/src/day15.rs
+++ 2019/rust/src/day15.rs
@@ -1,57 +1,186 @@
-use std::collections::HashMap;
+use slotmap::new_key_type;
 
 use crate::intcode::*;
+use std::{
+    cell::RefCell,
+    collections::{HashMap, VecDeque},
+    rc::Rc,
+};
 
 pub fn run_day_fifteen() {
     println!("Day 15: TODO");
 }
 
 fn inputs() -> Vec<MemoryCell> {
-    crate::util::read_int_array("inputs/day13.txt")
+    crate::util::read_int_array("inputs/day15.txt")
 }
 
-struct DroidInput {
-    next_input: Option<MemoryCell>,
+type DroidComputer = Computer<Vec<MemoryCell>, Rc<RefCell<DroidIo>>, Rc<RefCell<DroidIo>>>;
+
+struct Droid {
+    computer: DroidComputer,
+    map: HashMap<Point, Observation>,
+    location: Point,
+    path_to: HashMap<Point, PathNode>,
 }
 
-impl InputSource for DroidInput {
-    fn next(&mut self) -> Option<MemoryCell> {
-        self.next_input.take()
-    }
+struct PathNode {
+    direction: Direction,
+    from: Point,
 }
 
-struct DroidOutput {
-    last_output: Option<MemoryCell>,
-}
+impl Droid {
+    fn new(program: Vec<MemoryCell>) -> Droid {
+        let io = Rc::new(RefCell::new(DroidIo {
+            next_input: None,
+            next_output: None,
+        }));
 
-impl OutputSink for DroidOutput {
-    fn write(&mut self, value: MemoryCell) {
-        match self.last_output {
-            Some(_) => panic!("Output buffer is full!"),
-            None => self.last_output = Some(value),
+        let mut computer: DroidComputer = Computer::new(program, io.clone(), io);
+        computer.enable_extra_memory();
+
+        Self {
+            computer,
+            map: HashMap::new(),
+            location: ZERO_POINT,
+            path_to: HashMap::new(),
         }
     }
+
+    fn find_oxygen_bfs(&mut self) -> Option<Point> {
+        // Mark 0,0 as explored
+        self.map.insert(ZERO_POINT, Observation::Empty);
+
+        let mut candidates: VecDeque<Point> = VecDeque::new();
+
+        // Initialise candidates with the neighbours of 0,0
+
+        let mut path: VecDeque<Direction> = VecDeque::new();
+        while let Some(next) = candidates.pop_back() {
+            // Determine the path to the candidate
+            path.clear();
+            self.build_path_to(&next, &mut path);
+
+            // Start at 0,0
+            // Follow the path to the candidate
+            let mut observation = Observation::Empty;
+            for &d in path.iter() {
+                observation = self.traverse(d);
+            }
+
+            // Check for oxygen
+            if observation == Observation::OxygenSystem {
+                return Some(self.location);
+            }
+
+            // Update the map
+            self.map.insert(self.location, observation);
+
+            // Register any new candidates
+            // Return to 0,0
+            self.backtrack(&path);
+        }
+        todo!()
+    }
+
+    fn backtrack(&mut self, path: &VecDeque<Direction>) {
+        for d in path {
+            let _ = match d {
+                Direction::North => self.traverse(Direction::South),
+                Direction::South => self.traverse(Direction::North),
+                Direction::East => self.traverse(Direction::West),
+                Direction::West => self.traverse(Direction::East),
+            };
+        }
+    }
+
+    fn traverse(&mut self, direction: Direction) -> Observation {
+        let input = match direction {
+            Direction::North => 1,
+            Direction::South => 2,
+            Direction::West => 3,
+            Direction::East => 4,
+        };
+
+        {
+            let mut io = self.computer.input().borrow_mut();
+            io.next_input = Some(input);
+        }
+
+        let result = self.computer.resume();
+        match result {
+            Ok(StepResult::WaitingOnInput) => (),
+            _ => panic!("Computer didn't traverse as expected"),
+        }
+
+        let observation = {
+            let mut io = self.computer.output().borrow_mut();
+            io.next_output.take()
+        };
+
+        let observation = match observation {
+            Some(0) => Observation::Wall,
+            Some(1) => Observation::Empty,
+            Some(2) => Observation::OxygenSystem,
+            Some(_) => panic!("Unexpected output from computer"),
+            None => panic!("Computer didn't produce output"),
+        };
+
+        if observation != Observation::Wall {
+            let movement = match direction {
+                Direction::North => Point::xy(0, 1),
+                Direction::South => Point::xy(0, -1),
+                Direction::East => Point::xy(1, 0),
+                Direction::West => Point::xy(-1, 0),
+            };
+            self.location = self.location + movement;
+        }
+
+        observation
+    }
+
+    fn build_path_to(&self, destination: &Point, buffer: &mut VecDeque<Direction>) -> Option<()> {
+        let mut location = destination;
+        while location.x != 0 && location.y != 0 {
+            let path = self.path_to.get(location)?;
+            buffer.push_front(path.direction);
+            location = &path.from;
+        }
+        Some(())
+    }
 }
 
-type DroidComputer = Computer<Vec<MemoryCell>, DroidInput, DroidOutput>;
+struct DroidIo {
+    next_input: Option<MemoryCell>,
+    next_output: Option<MemoryCell>,
+}
+
+impl InputSource for Rc<RefCell<DroidIo>> {
+    fn next(&mut self) -> Option<MemoryCell> {
+        let mut io = self.borrow_mut();
+        io.next_input.take()
+    }
+}
+
+impl OutputSink for Rc<RefCell<DroidIo>> {
+    fn write(&mut self, value: MemoryCell) {
+        let mut io = self.borrow_mut();
+        io.next_output = Some(value);
+    }
+}
 
 type Point = crate::util::Point<MemoryCell>;
 
 const ZERO_POINT: Point = Point::xy(0, 0);
 
-struct Orchestrator {
-    computer: DroidComputer,
-    location: Point,
-    oxygen: Option<Point>,
-    observations: HashMap<Point, Observation>,
-}
-
+#[derive(Clone, Copy, PartialEq)]
 enum Observation {
     Empty,
     Wall,
     OxygenSystem,
 }
 
+#[derive(Clone, Copy)]
 enum Direction {
     North,
     South,
@@ -59,26 +188,9 @@ enum Direction {
     West,
 }
 
+#[derive(Clone, Copy, PartialEq)]
 enum MoveOutcome {
     ArrivedAtDestination,
     MoveSuccessful,
     StruckWall,
-}
-
-impl Orchestrator {
-    fn new(state: Vec<MemoryCell>) -> Orchestrator {
-        let input = DroidInput { next_input: None };
-        let output = DroidOutput { last_output: None };
-        let computer = DroidComputer::new(state, input, output);
-        Orchestrator {
-            computer,
-            location: ZERO_POINT,
-            oxygen: None,
-            observations: HashMap::new(),
-        }
-    }
-
-    fn move_droid(&mut self, direction: Direction) -> MoveOutcome {
-        unimplemented!();
-    }
 }
